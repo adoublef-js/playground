b8ff60e6354d7f12ec79ff7aa5ec0e5859952110
Kristopher Rahim <kristopherab@gmail.com>
2023-08-08 09:54:58 +0100

    add deps

---
+++ deps/deno_kv_oauth.ts
@@ -0,0 +1,8 @@
+export {
+    createAuth0OAuth2Client,
+    getSessionAccessToken,
+    getSessionId,
+    handleCallback,
+    signIn,
+    signOut,
+} from "https://deno.land/x/deno_kv_oauth@v0.3.0/mod.ts";
