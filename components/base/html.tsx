496c873957604d0a4f2d8b403d027bcc708a7ef3
Kristopher Rahim <kristopherab@gmail.com>
2023-08-12 08:07:53 +0100

    update html component

--- components/base/html.tsx
+++ components/base/html.tsx
@@ -1,30 +1,57 @@
 import { Component, html, memo } from "$deps/hono.ts";
+import { isAsync } from "https://deno.land/x/zod@v3.21.4/helpers/parseUtil.ts";
 
 type HtmlProps = {
     title?: string;
     lang?: "en";
     children?: Component | Component[];
+    class?: string;
 };
 
+const scripts = [
+    { src: "/htmx/@1.9.4/index.min.js", defer: false, async: false },
+    { src: "/htmx/@1.9.4/ws.js", defer: true, async: false },
+    // "/static/htmx/@1.9.4/sse.js",
+    // "/static/hyperscript/@0.9.11/index.min.js"
+];
+
+const styles = [
+    "/css/resets/elly-loel.css",
+    "/css/@0.0.1/debug.css",
+    "/css/@0.0.1/index.css",
+];
+
 export const Html = memo(
-    ({ children, title, lang = "en" }: HtmlProps) => html`
+    ({ children, title, ["class"]: className, lang = "en" }: HtmlProps) => html`
         <html>
             <head lang="${lang}">
-                <meta
-                    http-equiv="Content-Type"
-                    content="text/html;charset=UTF-8"
-                />
+                <meta content="utf-8" />
                 <meta
                     name="viewport"
                     content="width=device-width, initial-scale=1.0"
                 />
                 <title>${title}</title>
-                <script src="https://unpkg.com/htmx.org@1.9.4"></script>
-                <script src="https://unpkg.com/hyperscript.org@0.9.11"></script>
-                <script src="https://unpkg.com/htmx.org/dist/ext/sse.js"></script>
-                <script src="https://unpkg.com/htmx.org/dist/ext/ws.js"></script>
+                <!-- links to styles -->
+                ${styles.map(
+                    (href) => html`
+                        <link rel="preload" href="${href}" as="style" />
+                        <link rel="stylesheet" href="${href}" />
+                    `
+                )}
+                <!-- scripts -->
+                ${scripts.map(({ src, defer, async }) => {
+                    if (defer) {
+                        return html`<script src="${src}" defer></script>`;
+                    }
+
+                    if (async) {
+                        return html`<script src="${src}" async></script>`;
+                    }
+
+                    return html`<script src="${src}"></script>`;
+                })}
             </head>
-            <body id="root">
+            <body class="${className}">
                 ${children}
             </body>
         </html>
